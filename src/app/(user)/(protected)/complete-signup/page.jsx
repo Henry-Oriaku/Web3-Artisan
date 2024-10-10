3df7c8f39d40a10bb3d852ec9c3b66678790ef0c
Henry Oriaku <89471497+Henry-Oriaku@users.noreply.github.com>
2024-10-10 08:21:26 +0100

    Update page.jsx

--- src/app/(user)/(protected)/complete-signup/page.jsx
+++ src/app/(user)/(protected)/complete-signup/page.jsx
@@ -3,13 +3,13 @@ import { PlusIcon } from '@heroicons/react/24/outline';
 import { Button, Input, Modal } from 'antd'
 import Title from 'antd/es/typography/Title'
 import axios from 'axios';
-import GradientBg from 'components/gradient-bg';
-import AddEditService from 'components/user/service/add-edit-service';
-import ServiceCard from 'components/user/service/service-card';
+import GradientBg from '@/components/gradient-bg';
+import AddEditService from '@/components/user/service/add-edit-service';
+import ServiceCard from '@/components/user/service/service-card';
 import { ConnectKitButton } from 'connectkit'
-import apiUrl from 'constants/apiUrl';
-import webRoutes from 'constants/webRoutes';
-import { validateForm } from 'helpers/validator';
+import apiUrl from '@/constants/apiUrl';
+import webRoutes from '@/constants/webRoutes';
+import { validateForm } from '@/helpers/validator';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
